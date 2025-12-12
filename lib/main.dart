85b897759cbde0cc63d5da2bf0a20c960d921cf3
fastblacktiger2024-hash <fastblacktiger2024@gmail.com>
2025-12-13 03:59:32 +0400

    Update main.dart to support localization and locale provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
+import 'l10n/app_localizations.dart';
 import 'providers/product_provider.dart';
 import 'providers/cart_provider.dart';
+import 'providers/locale_provider.dart';
 import 'screens/splash_screen.dart';
 import 'screens/auth_screen.dart';
 import 'screens/home_screen.dart';
@@ -27,29 +29,35 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => ProductProvider()),
         ChangeNotifierProvider(create: (_) => CartProvider()),
+        ChangeNotifierProvider(create: (_) => LocaleProvider()),
       ],
-      child: MaterialApp(
-        title: 'فاست بلاك تايجر لقطع الغيار',
-        debugShowCheckedModeBanner: false,
-        locale: const Locale('ar'),
-        supportedLocales: const [Locale('ar')],
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        theme: ThemeData(
-          primarySwatch: Colors.orange,
-          fontFamily: 'Roboto',
-        ),
-        initialRoute: SplashScreen.routeName,
-        routes: {
-          SplashScreen.routeName: (_) => const SplashScreen(),
-          AuthScreen.routeName: (_) => const AuthScreen(),
-          HomeScreen.routeName: (_) => const HomeScreen(),
-          ProductDetailScreen.routeName: (_) => const ProductDetailScreen(),
-          CartScreen.routeName: (_) => const CartScreen(),
-          AdminScreen.routeName: (_) => const AdminScreen(),
+      child: Consumer<LocaleProvider>(
+        builder: (context, localeProvider, _) {
+          return MaterialApp(
+            title: 'فاست بلاك تايجر لقطع الغيار',
+            debugShowCheckedModeBanner: false,
+            locale: localeProvider.locale,
+            supportedLocales: const [Locale('ar'), Locale('en')],
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            theme: ThemeData(
+              primarySwatch: Colors.orange,
+              fontFamily: 'Roboto',
+            ),
+            initialRoute: SplashScreen.routeName,
+            routes: {
+              SplashScreen.routeName: (_) => const SplashScreen(),
+              AuthScreen.routeName: (_) => const AuthScreen(),
+              HomeScreen.routeName: (_) => const HomeScreen(),
+              ProductDetailScreen.routeName: (_) => const ProductDetailScreen(),
+              CartScreen.routeName: (_) => const CartScreen(),
+              AdminScreen.routeName: (_) => const AdminScreen(),
+            },
+          );
         },
       ),
     );
